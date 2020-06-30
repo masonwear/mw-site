43f33fd6bb02d2134403f15f725c12cb2e98b327
Mason Wear <mason.wear@gmail.com>
2020-06-30 13:09:25 -0500

    clean-up

--- src/pages/app.js
+++
@@ -1,9 +0,0 @@
-import { StaticKitProvider } from '@statickit/react';
-function App({ Component, pageProps }) {
-  return (
-    <StaticKitProvider site="3ce701dad217">
-      <Component {...pageProps} />
-    </StaticKitProvider>
-  );
-}
-export default App;
