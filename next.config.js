d9dce4e1d81d348997bdf5d2c4ecee8189f6ea47
Mason Wear <mason.wear@gmail.com>
2020-07-01 15:17:53 -0500

    Fathom Update

---
+++ next.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+
+    env: {
+
+        FATHOM_SITE_ID: 'LZWKJNEY',
+        FATHOM_CUSTOM_URL: 'https://masonwear.co/',
+
+    },
+}
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,15 +1,17 @@
 import App, { Container } from 'next/app';
 import React from 'react';
 import { DefaultSeo } from 'next-seo';
-import { Helmet } from 'react-helmet';
 import Fathom from '../components/Fathom'
 
 export default class MyApp extends App {
   render() {
     const { Component, pageProps } = this.props;
     return (
+       
       <Container>
-          <Fathom>  </Fathom>
+          <Fathom></Fathom>
+       
+           
         <DefaultSeo
           openGraph={{
             type: 'website',
@@ -18,7 +20,7 @@ export default class MyApp extends App {
             site_name: 'Mason Wear - Maker of Things',
             images: [
                 {
-                    url: 'https://masonwear.com/static/og-image.png',
+                    url: 'https://masonwear.co/static/og-image.png',
                     alt: 'Mason Wear',
                 },
 
@@ -30,21 +32,19 @@ export default class MyApp extends App {
             cardType: 'summary_large_image',
           }}
         />
+         
         <Component {...pageProps} />
       
-        <Helmet>
+        
           <title>Mason Wear - Maker of Things</title>
           <link rel="icon" type="image/x-icon" href="/static/favicon.ico" />
           <link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400&display=swap" rel="stylesheet" />
           <link href="https://fonts.googleapis.com/css2?family=Domine:wght@700&display=swap" rel="stylesheet" />
           
-          
-      
-          
-          </Helmet>
-       
+         
       
       </Container>
+     
 
 
     );
